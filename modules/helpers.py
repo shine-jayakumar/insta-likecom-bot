db2944d75a75547eed3bbf16ad9f3107f944e992
shine-jayakumar <shinejayakumar@yahoo.com>
2023-11-30 23:23:01 +0530

    Moved helper methods to this file

--- modules/helpers.py
+++ modules/helpers.py
@@ -1,3 +1,15 @@
+""" 
+    helpers.py - helper methods
+
+    insta-likecom-bot v.3.0.4
+    Automates likes and comments on an instagram account or tag
+
+    Author: Shine Jayakumar
+    Github: https://github.com/shine-jayakumar
+    Copyright (c) 2023 Shine Jayakumar
+    LICENSE: MIT
+"""
+
 
 from typing import List, Tuple
 import random
